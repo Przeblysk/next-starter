42862ee3b636b45672e7904468a18b78ac0780a2
Przeblysk <przeblysk@163.com>
2024-12-24 13:47:08 +0800

    chore: home page description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,12 @@ export default function Home() {
   return (
     <div className="container mx-auto w-full min-h-[calc(100vh-64px)]">
       <section className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <h1 className="text-5xl font-bold  text-center">
+        <h1 className="text-5xl font-bold  text-center mb-2">
           {t("HomePage.title")}
         </h1>
+        <p className="max-w-2xl text-center text-muted-foreground">
+          {t("HomePage.description")}
+        </p>
       </section>
     </div>
   )
