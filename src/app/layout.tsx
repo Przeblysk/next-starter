9fcbf8046f81ee2d89449a0e4de14f660adc8303
Przeblysk <przeblysk@163.com>
2025-03-20 13:20:12 +0800

    chore: update package dependencies and remove unused font imports in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import { ThemeProvider } from "@/components/theme-provider"
 import { NextIntlClientProvider } from "next-intl"
 import { getLocale, getMessages } from "next-intl/server"
-import { Geist, Geist_Mono } from "next/font/google"
 import { PropsWithChildren } from "react"
 
 import "./globals.css"
@@ -10,16 +9,6 @@ import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { Toaster } from "@/components/ui/toaster"
 import { createMetadata } from "@/lib/metadata"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"]
-})
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"]
-})
-
 export const metadata = createMetadata({})
 
 export default async function RootLayout({ children }: PropsWithChildren) {
@@ -27,8 +16,7 @@ export default async function RootLayout({ children }: PropsWithChildren) {
   const messages = await getMessages()
   return (
     <html lang={locale} suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`antialiased`}>
         <NextIntlClientProvider messages={messages}>
           <ThemeProvider attribute="class">
             {children}
